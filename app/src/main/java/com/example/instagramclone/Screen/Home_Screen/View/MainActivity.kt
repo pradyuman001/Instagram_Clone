a9216d8f7eb60cc6435dec03530f2fd6fc9d3703
Pradyuman <pradyumangodhani006@gmail.com>
2022-05-18 20:22:42 +0530

    18-05-2022

--- app/src/main/java/com/example/instagramclone/Screen/Home_Screen/View/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/Screen/Home_Screen/View/MainActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+import com.example.instagramclone.Fragments.*
 import com.example.instagramclone.R
 import com.example.instagramclone.Screen.Home_Screen.Controller.MyViewPager_Adapter
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var activity_btn: Button
-    lateinit var reels_btn: Button
-    lateinit var search_btn: Button
-    lateinit var home_btn: Button
+    lateinit var profile_img: ImageView
+    lateinit var activity_img: ImageView
+    lateinit var reels_img: ImageView
+    lateinit var search_img: ImageView
+    lateinit var home_img: ImageView
     lateinit var frame_layout: FrameLayout
     lateinit var viewpager: ViewPager
 
@@ -25,68 +30,56 @@ class MainActivity : AppCompatActivity() {
 
         binding()
 
+/*
 
         var adapter_view = MyViewPager_Adapter(this,img);
         viewpager.adapter  = adapter_view;
+*/
 
+        loadfragment(Home_Fragment())
 
+        home_img.setOnClickListener {
 
+            loadfragment(Home_Fragment())
 
+            var toast = Toast.makeText(this, "Home", Toast.LENGTH_LONG)
+            toast.show()
 
+        }
 
+        search_img.setOnClickListener {
 
+            loadfragment(Search_Fragment())
 
+            var toast = Toast.makeText(this, "Search", Toast.LENGTH_LONG)
+            toast.show()
 
+        }
 
+        reels_img.setOnClickListener {
 
+            var toast = Toast.makeText(this, "Reels", Toast.LENGTH_LONG)
+            toast.show()
 
+            loadfragment(Reels_Fragment())
+        }
 
+        activity_img.setOnClickListener {
 
+            var toast = Toast.makeText(this, "Activity", Toast.LENGTH_LONG)
+            toast.show()
 
+            loadfragment(Activity_Fragment())
 
+        }
+        profile_img.setOnClickListener {
 
+            var toast = Toast.makeText(this, "Profile", Toast.LENGTH_LONG)
+            toast.show()
 
+            loadfragment(Profile_Fragment())
 
-
-
-
-
-//        loadfragment(Home_Fragment())
-//
-//        home_btn.setOnClickListener {
-//
-//            loadfragment(Home_Fragment())
-//
-//            var toast = Toast.makeText(this, "Home", Toast.LENGTH_LONG)
-//            toast.show()
-//
-//        }
-//
-//        search_btn.setOnClickListener {
-//
-//            loadfragment(Search_Fragment())
-//
-//            var toast = Toast.makeText(this, "Search", Toast.LENGTH_LONG)
-//            toast.show()
-//
-//        }
-//
-//        reels_btn.setOnClickListener {
-//
-//            var toast = Toast.makeText(this, "Reels", Toast.LENGTH_LONG)
-//            toast.show()
-//
-//            loadfragment(Reels_Fragment())
-//        }
-//
-//        activity_btn.setOnClickListener {
-//
-//            var toast = Toast.makeText(this, "Activity", Toast.LENGTH_LONG)
-//            toast.show()
-//
-//            loadfragment(Activity_Fragment())
-//
-//        }
+        }
 
 
     }
@@ -94,22 +87,25 @@ class MainActivity : AppCompatActivity() {
     fun binding() {
 
 
+/*
         viewpager=findViewById<ViewPager>(R.id.viewpager);
+*/
 
-//        frame_layout = findViewById<FrameLayout>(R.id.frame_layout)
-//        home_btn = findViewById<Button>(R.id.home_btn)
-//        search_btn = findViewById<Button>(R.id.search_btn)
-//        reels_btn = findViewById<Button>(R.id.reels_btn)
-//        activity_btn = findViewById<Button>(R.id.activity_btn)
+        frame_layout = findViewById<FrameLayout>(R.id.frame_layout)
+        home_img = findViewById<ImageView>(R.id.home_img)
+        search_img = findViewById<ImageView>(R.id.search_img)
+        reels_img = findViewById<ImageView>(R.id.reels_img)
+        activity_img = findViewById<ImageView>(R.id.activity_img)
+        profile_img = findViewById<ImageView>(R.id.profile_img)
 
     }
 
-//    fun loadfragment(fragment : Fragment){
-//
-//        var fragmentTransient = supportFragmentManager.beginTransaction()
-//        fragmentTransient.replace(R.id.frame_layout,fragment)
-//        fragmentTransient.commit()
-//
-//    }
+    fun loadfragment(fragment : Fragment){
+
+        var fragmentTransient = supportFragmentManager.beginTransaction()
+        fragmentTransient.replace(R.id.frame_layout,fragment)
+        fragmentTransient.commit()
+
+    }
 
 }
